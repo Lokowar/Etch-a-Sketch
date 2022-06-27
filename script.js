4ee205fd9615f02d578a2d98f5192c7df46d4aae
Lokowar <lokwoar1@gmail.com>
2022-06-27 17:23:35 -0300

    0.4 - Alterei o algoritmo de geração do grid

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
+    function gridCreation(column_n, row_n){
+        for(j = 0; j < column_n; j++){
+            const column = document.createElement("div");
+            column.className = "column";
+            $(column).appendTo(board);
+            for(i = 0; i < row_n; i++){
+                const grid = document.createElement("div");
+                grid.className = "grid";
+                $(grid).appendTo(column);
+            }
+        }
+        return;
+    }
+
     const board = document.getElementsByClassName("board");
+
     const button1 = document.getElementById("clear");
     const button2 = document.getElementById("erase");
     const button3 = document.getElementById("random");
+
+    const slider = document.getElementById("myRange");
 
     grid = document.getElementsByClassName("grid");
 
     let mode = 0;
 
-    for(i = 0; i < 18; i++){
-        const grid = document.createElement("div");
-        grid.className = "grid";
-        $(grid).appendTo(board);
-    }
+    let column_n = slider.value;
+    let row_n = slider.value;
+
+    gridCreation(column_n, row_n);
+    
 
     $(button1).click(function(){
         for(i = 0; i < 18; i++){
@@ -21,16 +38,13 @@ $(document).ready(function() {
     });
 
     $(button2).click(function(){
-        console.log("yay");
         if(mode === 0){
             mode = 1;
-            console.log("mode = 1");
             return;
         }
 
         if(mode === 1){
             mode = 0;
-            console.log("mode = 0");
             return;
         }
 
@@ -41,13 +55,13 @@ $(document).ready(function() {
     });
 
     $(button3).click(function(){
-        console.log("yay");
         mode = 2;
     });
 
     $(".grid").on('mouseover', function(){
         if(mode === 0){
             $(this).css("background-color", "black");
+            console.log(slider.value);
         }
         if(mode === 1){
             $(this).css("background-color", "white");
